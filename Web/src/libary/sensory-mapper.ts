5be14b0da2a757c5d7186a00fac7958b8a2b7eeb
Justin McCrea <mammerjam303@gmail.com>
2025-11-30 01:28:00 -0700

    Add conditional return for high probability in mapper
    Signed-off-by: Justin McCrea <mammerjam303@gmail.com>

--- Web/src/libary/sensory-mapper.ts
+++ Web/src/libary/sensory-mapper.ts
@@ -7,3 +7,13 @@ const sensory = Mapper.mapSensory(prm);
 showRecommendation(sensory.recommendation);
 playTone(sensory.harmonic);
 vibrate(sensory.haptic);
+// src/sensory-mapper.ts
+if (probability >= 0.7) {
+  return {
+    harmonic: this.mapToRange(resonanceFreq, 1000, 1432),
+    haptic: 'Pulse (strong, confident)',
+    phonic: 'Sine (calm, smooth)',
+    recommendation: 'SEND',
+    // ...
+  };
+}
