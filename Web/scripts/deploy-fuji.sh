ec099d9309f0936e9fe15de2b02af9889ea956d2
Justin McCrea <mammerjam303@gmail.com>
2025-12-01 05:16:57 -0700

    Enhance deploy-fuji.sh for Fuji network deployment
    Added steps to deploy and bootstrap a subnet on the Fuji network, including creating the subnet, adding a validator, and deploying contracts.
    
    Signed-off-by: Justin McCrea <mammerjam303@gmail.com>

--- Web/scripts/deploy-fuji.sh
+++ Web/scripts/deploy-fuji.sh
@@ -17,3 +17,21 @@ avalanche subnet deploy rangis --config-dir config/ --local
 avalanche subnet bootstrap rangis --local
 
 echo "Local Subnet Ready! Test RPC: http://127.0.0.1:9650/ext/bc/rangis/rpc"
+# ... existing ...
+# Switch to Fuji
+avalanche config chain --network fuji
+
+# Create/deploy on Fuji
+avalanche subnet create rangis --fuji
+avalanche subnet deploy rangis --config-dir config/ --fuji
+
+# Add validator (use your Fuji-funded wallet)
+avalanche subnet add-validator rangis --signer-key-dir validators/ --fuji
+
+# Bootstrap
+avalanche subnet bootstrap rangis --fuji
+
+# Deploy contracts (e.g., HarmonicConcesus)
+forge script scripts/Deploy.s.sol --rpc-url https://api.avax-test.network/ext/bc/C/rpc --private-key $FUJI_PK --broadcast
+
+echo "Fuji Subnet Live! RPC: https://subnets.avax.network/rangis/rpc (check explorer)"
